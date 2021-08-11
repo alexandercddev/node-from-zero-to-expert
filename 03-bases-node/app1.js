31e0f7829577151dbf28730cedaebd339943e2ca
Alexander Chi <arturochi2@hotmail.com>
2021-08-11 17:04:35 -0500

    Se agregó base node

--- 03-bases-node/app1.js
+++ 03-bases-node/app1.js
@@ -5,11 +5,15 @@
  */ 
 const {createFile} = require('./helpers/mutiplicar')
 
- console.clear(); 
- const base = 3; 
+console.clear(); 
+
+
+
+
+ /*const base = 3; 
  createFile(base).then((result)=> {
     console.log(result, 'creado')
  }).catch((err) => {
     console.log(err)
  });
- 
+ */
